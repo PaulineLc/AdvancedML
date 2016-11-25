7a17da9e9cf635e2b0504629bfdfcf0859a18e38
Pauline Lallinec <PaulineLc@users.noreply.github.com>
2016-11-25 18:54:20 +0000

    minor performance improvement

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -9,8 +9,8 @@ class TestCosineCalculation(unittest.TestCase):
     def test_cosine1(self):
         # Create a document for the sentence "world cup winners 2014'
         # term id 1 = 'world', 2 = 'cup', 3 = 'winners', 4 = '2014', 5 = '2016'
-        d1 = Document(1)
-        d1.bag_of_words = {1: 1, 2: 1, 3: 1, 4: 1}
+        d1 = 1
+        d1_bag_of_words = {1: 1, 2: 1, 3: 1, 4: 1}
 
         # Create a document for the sentence "cup winners 2016"
         d2 = Document(2)
@@ -18,6 +18,7 @@ class TestCosineCalculation(unittest.TestCase):
 
         # creating mock objects
         clf = TextClassifier(d1)
+        clf.document.bag_of_words = d1_bag_of_words
         clf.all_bags_of_words[2] = d2.bag_of_words
         cosine = clf.calculate_cosine(2)
 
@@ -29,21 +30,22 @@ class TestCosineCalculation(unittest.TestCase):
         # 8: 'costly', 9: 'vehicle', 10: 'the', 11: 'feline', 12: 'animal'
 
         # d1 sentence: "jaguars are expensive cars"
-        d1 = Document(1)
-        d1.bag_of_words = {1:1, 2:1, 3:1, 4: 1}
+        d1 = 1
+        d1_bag_of_words = {1:1, 2:1, 3:1, 4: 1}
 
         # d2 sentence: "a jaguar is a costly vehicle"
-        d2 = Document(2)
-        d2.bag_of_words = {5:1, 6:1, 7:1, 8:1, 9:1}
+        d2 = 2
+        d2_bag_of_words = {5:1, 6:1, 7:1, 8:1, 9:1}
 
         # d2 sentence: "the jaguar, a feline animal"
-        d3 = Document(3)
-        d3.bag_of_words = {10:1, 6:1, 5:1, 11:1, 12:1}
+        d3 = 3
+        d3_bag_of_words = {10:1, 6:1, 5:1, 11:1, 12:1}
 
         # creating mock objects
         clf = TextClassifier(d2)
-        clf.all_bags_of_words[1] = d1.bag_of_words
-        clf.all_bags_of_words[3] = d3.bag_of_words
+        clf.document.bag_of_words = d2_bag_of_words
+        clf.all_bags_of_words[d1] = d1_bag_of_words
+        clf.all_bags_of_words[d3] = d3_bag_of_words
 
         # calculating cosine
         cos_d1 = clf.calculate_cosine(1)
@@ -58,16 +60,17 @@ class TestCosineCalculation(unittest.TestCase):
         # 14:and, 15:she, 16:pretty, 17:very
 
         # d1 sentence: zoe is the prettiest cat that has ever been or will ever be
-        d1 = Document(1)
-        d1.bag_of_words = {1:1, 2:1, 3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1, 11:1, 12:1}
+        d1 = 1
+        d1_bag_of_words = {1:1, 2:1, 3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1, 11:1, 12:1}
 
         # d2 sentence: zoe is my cat and she is very pretty
-        d2 = Document(2)
-        d2.bag_of_words = {1:1, 2:1, 13:1, 5:1, 14:1, 15:1, 17:1, 16:1}
+        d2 = 2
+        d2_bag_of_words = {1:1, 2:1, 13:1, 5:1, 14:1, 15:1, 17:1, 16:1}
 
         # creating mock objects
         clf = TextClassifier(d1)
-        clf.all_bags_of_words[2] = d2.bag_of_words
+        clf.document.bag_of_words = d1_bag_of_words
+        clf.all_bags_of_words[d2] = d2_bag_of_words
 
         # calculating cosine
         cosine = clf.calculate_cosine(2)
@@ -77,14 +80,13 @@ class TestCosineCalculation(unittest.TestCase):
     def test_cosine4(self):
         # last example taken from:
         # https://stackoverflow.com/questions/1746501/can-someone-give-an-example-of-cosine-similarity-in-a-very-simple-graphical-wa
-        d1 = Document(1)
-        d1.bag_of_words = {'Julie': 1, 'Linda': 1, 'loves': 2, 'me': 2, 'more': 1, 'than': 1}
 
         d2 = Document(2)
         d2.bag_of_words = {'Jane': 1, 'Julie': 1, 'likes': 1, 'loves': 1, 'me': 2, 'more': 1, 'than': 1}
 
         # creating mock classifier
-        clf = TextClassifier(d1)
+        clf = TextClassifier(1)
+        clf.document.bag_of_words = {'Julie': 1, 'Linda': 1, 'loves': 2, 'me': 2, 'more': 1, 'than': 1}
         clf.all_bags_of_words[2] = d2.bag_of_words
 
         self.assertAlmostEqual(clf.calculate_cosine(2), 0.822, places=3)
