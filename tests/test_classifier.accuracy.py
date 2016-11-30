b0ad5acdc4e6ba81b2dd02004b6831d7dc2e2cac
Pauline Lallinec <PaulineLc@users.noreply.github.com>
2016-11-30 19:47:28 +0000

    created a test for the accuracy, test passed

---
+++ tests/test_classifier.accuracy.py
@@ -0,0 +1,41 @@
+import unittest
+import pandas as pd
+
+from Assignment.classifier import TextClassifier
+from Assignment.document import Document
+from Assignment.dataset import Dataset
+
+
+class ClassifierClassify(unittest.TestCase):
+
+    def test_classification_training_set(self):
+        # creating mock database
+        Dataset.article_data = pd.DataFrame({'doc_id': [1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3],
+                                             'term_id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 5, 11, 12],
+                                             'nb_occurrences': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]})
+
+        Dataset.article_labels = pd.DataFrame({'doc_id': [1, 2, 3], 'class': ['car', 'car', 'animal']})
+
+        accuracy = TextClassifier.calculate_accuracy(0, 0.67)
+
+        if 2 in list(TextClassifier.test_set) or 3 in list(TextClassifier.test_set):
+            self.assertEqual(accuracy, (0, 0))
+        else:
+            self.assertEqual(accuracy, (1, 1))
+
+    def test_classification_training_set(self):
+        # creating mock database
+        Dataset.article_data = pd.DataFrame({'doc_id': [1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3],
+                                             'term_id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 5, 11, 12],
+                                             'nb_occurrences': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]})
+
+        Dataset.article_labels = pd.DataFrame({'doc_id': [1, 2, 3], 'class': ['car', 'car', 'animal']})
+
+        accuracy = TextClassifier.calculate_accuracy(1, 3)
+
+        self.assertAlmostEqual(accuracy[0], 0.33, places=1)
+        self.assertAlmostEqual(accuracy[1], 0.33, places=1)
+
+
+if __name__ == '__main__':
+    unittest.main()
